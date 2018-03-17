fc6a74ce5d602746098f577c3d23bd024460153b
NotBrianZach <brianzacabel@utexas.edu>
2018-03-16 19:43:51 -0500

    implementation hangs

--- bZachA/Euler3/3.hs
+++ bZachA/Euler3/3.hs
@@ -34,57 +34,49 @@ import Data.Int
 --   (_,_,c) <- get
 --   return c
 
-sieveOfFactoring :: Int -> Int -> [Int] -> Int
-sieveOfFactoring inputNumber maxExtentPrimeSieveList initialListOfPrimes =
-  if inputNumber `rem` numberToTest == 0 then
-    if inputNumber == numberToTest * (product testedPrimes) then
+-- indexOfPrimeList should always start at 0, list ofPrimes as [], maxExtentPrimeSieveList > 2, listOfFactors [1]
+sieveOfFactoring :: Int -> [Int] -> Int -> [Int] -> Int -> Int
+sieveOfFactoring indexOfPrimeList listOfPrimes maxExtentPrimeSieveList listOfFactors inputNumber =
+  -- branch prediction bless us with your power, save us from our sins
+  if null listOfPrimes then
+      let newListOfPrimes = [2.. maxExtentPrimeSieveList] in
+      if inputNumber `rem` newListOfPrimes !! indexOfPrimeList == 0 then
+        if inputNumber == newListOfPrimes !! indexOfPrimeList * product listOfFactors then
+            head $ tail listOfFactors
+          else
+            sieveOfFactoring indexOfPrimeList (filter (\num -> (num `rem` newListOfPrimes !! indexOfPrimeList) /= 0) newListOfPrimes) maxExtentPrimeSieveList ((newListOfPrimes !! indexOfPrimeList):listOfFactors) inputNumber
       else
-    sieveOfFactoring
-  else
-  where numberToTest = inputNumber + 1
-
-
-  -- flip evalState (3, [2..maxExtentPrimeSieveList], []) $ do
-  -- _ <- forM [0..(maxExtentPrimeSieveList/2)] $ \_ -> do
-  --   (numberToTest, testedPrimes, inputPrimeFactors) <- get
-  --   if inputNumber `rem` numberToTest == 0 then
-  --     if inputNumber == numberToTest * (product testedPrimes) then
-  --         put (numberToTest + 1, [], numberToTest:testedPrimes)
-  --       else
-  --         put (numberToTest + 1, [], numberToTest:testedPrimes)
-  --     else
-  --       put (numberToTest + 1, [], numberToTest:testedPrimes)
-  -- (_,_,c) <- get
-  -- return c
-
-
-fib :: Int64 -> Int64
-fib n = flip evalState (0,1,0) $ do
-  _ <- forM [0..(n-1)] $ \_ -> do
-    (a,b,c) <- get
-    if a+b > 4000000 then
-      put (a,b,c)
+        sieveOfFactoring indexOfPrimeList (filter (\num -> (num `rem` newListOfPrimes !! indexOfPrimeList) /= 0) newListOfPrimes) maxExtentPrimeSieveList listOfFactors inputNumber
+    else
+      if inputNumber `rem` listOfPrimes !! indexOfPrimeList == 0 then
+        if inputNumber == listOfPrimes !! indexOfPrimeList * product listOfFactors then
+            head $ tail listOfFactors
+          else
+            sieveOfFactoring indexOfPrimeList (filter (\num -> (num `rem` listOfPrimes !! indexOfPrimeList) /= 0) listOfPrimes) maxExtentPrimeSieveList (listOfPrimes !! indexOfPrimeList:listOfFactors) inputNumber
       else
-      if even (a+b) then
-        put (b,a+b, c+a+b)
-        else
-        put (b, a+b, c)
-  (_,_,c) <- get
-  return c
-
+        sieveOfFactoring indexOfPrimeList (filter (\num -> (num `rem` listOfPrimes !! indexOfPrimeList) /= 0) listOfPrimes) maxExtentPrimeSieveList listOfFactors inputNumber
 
 main :: IO ()
 main = do
-  upperBoundInput <- getLine
-  let upperBound = read upperBoundInput ::Int64
+  putStrLn "enter indexOfPrimeList"
+  indexOfPrimeListInput <- getLine
+  putStrLn "enter listOfPrimes"
+  listOfPrimesInput <- getLine
+  putStrLn "enter maxExtentPrimeSieveList"
+  maxExtentPrimeSieveListInput <- getLine
+  putStrLn "enter listOfFactors"
+  listOfFactorsInput <- getLine
+  putStrLn "enter inputNumber"
+  inputNumberInput <- getLine
+
+  let indexOfPrimeList = read indexOfPrimeListInput ::Int
+  let listOfPrimes = read listOfPrimesInput ::[Int]
+  let maxExtentOfPrimeSieveList = read maxExtentPrimeSieveListInput ::Int
+  let listOfFactors = read listOfFactorsInput ::[Int]
+  let inputNumber = read inputNumberInput ::Int
+
   -- let listOfFibs = takeWhile (>= upperBound) [ fib  | i <- [1..]]
   -- foldl'
 
-  -- let theAnswer = foldl (\base numToTest ->
-  --                           if numToTest `rem` 5 == 0 || numToTest `rem` 3 == 0 then
-  --                             base + numToTest
-  --                           else
-  --                             base
-  --                           ) 0 [1..999]
-  putStrLn $ "upper bound is " ++ show upperBound
-  putStrLn $ "The answer is " ++ show (fib upperBound)
+  let theAnswer = sieveOfFactoring indexOfPrimeList listOfPrimes maxExtentOfPrimeSieveList listOfFactors inputNumber
+  putStrLn $ "the larget prime factor of" ++ show inputNumber  ++ " is " ++ show theAnswer
